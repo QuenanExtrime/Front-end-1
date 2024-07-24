92c94225983c624657703c02dc32f9509d792785
QuenanExtrime <quenan2009@gmail.com>
2024-07-23 22:50:38 -0300

    CODIGOS AULA 23

---
+++ Event Listeners/index.js
@@ -0,0 +1,14 @@
+// document.querySelector(button).addEventListener(click, function (){
+//     alert(Você clicou no botão!)
+// })
+
+// // const mouseHandler = () => {
+// //     document.querySelector(h1).classlist.toogle(header)
+// // }
+
+// document.querySelector(h1).addEventListener(mouseenter, mouseHandler)
+// document.querySelector(h1).addEventListener(mouseout, mouseHandler)
+
+// Document.querySelector(input).addEventListener(keydown, function(event){
+//     console.log(event.target.value);
+// })
